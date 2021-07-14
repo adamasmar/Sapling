f9c59947480ea0acb7da038aa0bd6a6c3860dcfa
Adam Asmar <asad112@gmail.com>
2021-07-14 13:03:56 -0500

    Add project files.

---
+++ AdamSapling/TimeZoneHelper.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AdamSapling
+{
+    public class TimeZoneHelper : IPlugin
+    {
+        private readonly Guid _adminUserId;
+
+        public TimeZoneHelper()
+        {
+            //if (Guid.TryParse(adminUserId, out var result))
+            //{
+            //    _adminUserId = result;
+            //}
+            //else
+            //{
+            //    throw new InvalidPluginExecutionException("The admin user is missing");
+            //}
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider.GetService(typeof(IPluginExecutionContext)) is IPluginExecutionContext context &&
+                serviceProvider.GetService(typeof(IOrganizationServiceFactory)) is IOrganizationServiceFactory serviceFactory &&
+                serviceProvider.GetService(typeof(ITracingService)) is ITracingService tracing &&
+                serviceFactory.CreateOrganizationService(context.UserId) is IOrganizationService service &&
+                serviceFactory?.CreateOrganizationService(_adminUserId) is IOrganizationService adminService &&
+                context.PrimaryEntityName == "adamsap_school")
+            {
+                //var schoolAddressEntity = GetAddressInfo(adminService, context.PrimaryEntityId.ToString());
+                //var restSetting = GetRestInfo(adminService, "BingGetPoint");
+
+                //if (restSetting == null)
+                //{
+                //    throw new InvalidPluginExecutionException("REST API Data not present");
+                //}
+
+                //var apiData = RestApiCall(restSetting, tracing);
+                var apiData = RestApiCall(null, tracing);
+
+                //EnterNoteRecord(service, "User service", apiData, regarding, tracing);
+                //EnterNoteRecord(adminService, "Admin Service", apiData, regarding, tracing);
+            }
+        }
+
+        private static Entity GetAddressInfo(IOrganizationService adminService, string id)
+        {
+            var restInfo = new Entity();
+
+            var query = new QueryExpression()
+            {
+                Distinct = true,
+                EntityName = "adamsap_school",
+                ColumnSet = new ColumnSet("adamsap_schooladdressstreet", "adamsap_schooladdresscity",
+                    "adamsap_schoolstate", "adamsap_schooladdresszip"),
+                Criteria =
+                {
+                    Filters =
+                    {
+                        new FilterExpression
+                        {
+                            FilterOperator = LogicalOperator.And,
+                            Conditions =
+                            {
+                                new ConditionExpression("adamsap_schoolid", ConditionOperator.Equal, id )
+                            }
+                        }
+                    }
+                }
+            };
+
+            var results = adminService.RetrieveMultiple(query);
+
+            if (results.Entities.Count > 0)
+            {
+                restInfo = results.Entities[0];
+            }
+
+            return restInfo;
+        }
+
+        private static Entity GetRestInfo(IOrganizationService adminService, string serviceName)
+        {
+            var restInfo = new Entity();
+
+            var query = new QueryExpression
+            {
+                Distinct = true,
+                EntityName = "adamsap_envvarconfiguration",
+                ColumnSet = new ColumnSet("adamsap_host", "adamsap_url", "adamsap_key"),
+                Criteria =
+                {
+                    Filters =
+                    {
+                        new FilterExpression
+                        {
+                            FilterOperator = LogicalOperator.And,
+                            Conditions =
+                            {
+                                new ConditionExpression("adamsap_host", ConditionOperator.Equal, serviceName )
+                            }
+                        }
+                    }
+                }
+            };
+
+            var results = adminService.RetrieveMultiple(query);
+
+            if (results.Entities.Count > 0)
+            {
+                restInfo = results.Entities[0];
+            }
+
+            return restInfo;
+        }
+
+        private static string RestApiCall(Entity restApiInfo, ITracingService tracing)
+        {
+            string result;
+
+            const string key = "AnKkRcs9vKmajbA4kfwIXwWi0BuhmUfye-zTdxmX8IPVy1ZaNplbaXrRzjL1YNgc";
+            const string query = "1 Microsoft Way, Redmond, WA";
+
+            var url = $"http://dev.virtualearth.net/REST/v1/Locations?q={query}";
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.Accept = "application/json";
+            request.Headers.Add("key", key);
+            request.ContentType = "text/plain";
+            request.PreAuthenticate = true;
+
+            //await Task.Delay(0);
+
+            try
+            {
+                using (var responseReader = new StreamReader(request.GetResponse().GetResponseStream() ??
+                                                             throw new InvalidOperationException(
+                                                                 "Unable to get REST response stream")))
+                {
+                    result = responseReader.ReadToEnd();
+                    tracing.Trace($"REST API results: {result}");
+                }
+            }
+            catch (WebException we)
+            {
+                var allomentNumber = string.Empty;
+                var streamData = new StringBuilder();
+
+                streamData.AppendLine("---HTTP Error---");
+
+                WebResponse webResponse = (HttpWebResponse)we.Response;
+
+                var stream = webResponse.GetResponseStream();
+                var encode = Encoding.GetEncoding("utf-8");
+                var readStream = new StreamReader(stream ?? throw new InvalidOperationException(), encode);
+                var read = new char[256];
+
+                var count = readStream.Read(read, 0, 256);
+
+                while (count > 0)
+                {
+                    // Dump the 256 characters on a string and display the string onto the console.
+                    var str = new string(read, 0, count);
+                    streamData.Append(str);
+                    count = readStream.Read(read, 0, 256);
+                }
+
+                stream.Close();
+                webResponse.Close();
+
+                result = streamData.ToString();
+
+            }
+            return result;
+        }
+
+        private void EnterNoteRecord(IOrganizationService service, string serviceType, string noteText,
+            EntityReference objectId, ITracingService tracing)
+        {
+            var note = new Entity("annotation");
+            note.Attributes.Add("subject", "The service " + serviceType);
+            note.Attributes.Add("notetext", noteText);
+            note.Attributes.Add("objectid", objectId);
+
+            var recordId = service.Create(note);
+            tracing.Trace($"New note ID is {recordId.ToString()}");
+        }
+    }
+}
---
+++ AdamSaplingWorkflows/Workflows/TimeZone.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Activities;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using AdamSaplingWorkflows.Models;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+
+// ReSharper disable UnusedMember.Global
+
+namespace AdamSaplingWorkflows.Workflows
+{
+    public class TimeZone : CodeActivity
+    {
+        // sets up input arguments
+        #region input arguments
+
+        [RequiredArgument]
+        [Input(nameof(InputSchoolAddressStreet))]
+        public InArgument<string> InputSchoolAddressStreet { get; set; }
+
+        [RequiredArgument]
+        [Input(nameof(InputSchoolAddressCity))]
+        public InArgument<string> InputSchoolAddressCity { get; set; }
+
+        [RequiredArgument]
+        [Input(nameof(InputSchoolAddressState))]
+        public InArgument<string> InputSchoolAddressState { get; set; }
+
+        [RequiredArgument]
+        [Input(nameof(InputSchoolAddressZip))]
+        public InArgument<string> InputSchoolAddressZip { get; set; }
+
+        #endregion
+        // sets up output arguments
+        #region output arguments
+
+        [Output(nameof(TimeZoneName))]
+        public OutArgument<string> TimeZoneName { get; set; }
+
+        #endregion
+
+        // internal variables
+        private ITracingService _tracingService;
+        private const string MissingTimeZoneValue = "Time zone not found based on provided address";
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            // initiates tracing service
+            _tracingService = context.GetExtension<ITracingService>();
+
+            TimeZoneName.Set(context, MissingTimeZoneValue);
+
+            // sets time zone
+
+            TimeZoneName.Set(context,
+                GetTimeZone(InputSchoolAddressStreet.Get<string>(context), InputSchoolAddressCity.Get<string>(context),
+                    InputSchoolAddressState.Get<string>(context), InputSchoolAddressZip.Get<string>(context)));
+        }
+
+        public string GetTimeZone(string inputSchoolAddressStreet = null,
+            string inputSchoolAddressCity = null, string inputSchoolAddressState = null,
+            string inputSchoolAddressZip = null)
+        {
+            // builds URI to be passed to the HTTP client
+            var uriBuilder = new UriBuilder(@"https://dev.virtualearth.net/REST/v1/TimeZone/");
+            var parsedQuery = HttpUtility.ParseQueryString(uriBuilder.Query);
+            parsedQuery["key"] = "[your bing API key]";
+            parsedQuery["query"] =
+                $@"{inputSchoolAddressStreet} {inputSchoolAddressCity} {inputSchoolAddressState} {inputSchoolAddressZip}";
+            uriBuilder.Query = parsedQuery.ToString();
+
+            try
+            {
+                using (var deSerializeMemoryStream = new MemoryStream())
+                {
+                    //JSON string that we get from web api 
+                    var responseString = new HttpClient().GetStringAsync(uriBuilder.Uri).Result;
+
+                    //initialize DataContractJsonSerializer object and pass TimeZoneModel class type to it
+                    var serializer = new DataContractJsonSerializer(typeof(TimeZoneModel));
+                    
+                    //user stream writer to write JSON string data to memory stream
+                    var writer = new StreamWriter(deSerializeMemoryStream);
+                    writer.Write(responseString);
+                    writer.Flush();
+                    deSerializeMemoryStream.Position = 0;
+
+                    //get the Deserialized data in object of type TimeZoneModel
+                    var serializedObject = serializer.ReadObject(deSerializeMemoryStream) as TimeZoneModel;
+
+                    return serializedObject?.ResourceSets.FirstOrDefault() is ResourceSet resourceSet &&
+                           resourceSet.Resources.FirstOrDefault() is Resource resource &&
+                           resource.TimeZoneAtLocation.FirstOrDefault() is TimeZoneAtLocation timeZoneAtLocation &&
+                           timeZoneAtLocation.TimeZone.FirstOrDefault() is Timezone timeZoneElement &&
+                           timeZoneElement.GenericName is string genericName
+                        ? genericName
+                        : MissingTimeZoneValue;
+                }
+            }
+            catch (Exception exception)
+            {
+                // if an exception is thrown, log the exception and then return the default value for missing
+                var exceptionText = $"An exception occurred: {exception.Message} ({exception.GetType()}) (uri = {uriBuilder.Uri})";
+                Console.WriteLine(exceptionText);
+                _tracingService?.Trace(exceptionText);
+                return MissingTimeZoneValue;
+            }
+        }
+    }
+}
